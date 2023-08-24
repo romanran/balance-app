0934563557419b3d12bb7e76f4996fcb60de53fa
Roman <romanranniew@gmail.com>
2023-08-24 05:39:56 +0200

    Improve balance test

--- src/services/helpers/__tests__/balance.spec.ts
+++ src/services/helpers/__tests__/balance.spec.ts
@@ -5,10 +5,10 @@ import { transformTransactions } from '../balance'
 
 describe('Transactions', () => {
     const transactions: Transaction[] = [
-        { date: '2022-10-12T06:06:22.118Z', amount: 400.1 },
-        { date: '2022-10-24T06:06:22.118Z', amount: -400.1 },
-        { date: '2022-11-24T06:06:22.118Z', amount: 100.42 },
-        { date: '2022-12-24T06:06:22.118Z', amount: -100.42 }
+        { date: '2022-10-02T06:02:22.000Z', amount: 400.1 },
+        { date: '2022-10-13T14:10:12.000Z', amount: -400.1 },
+        { date: '2022-11-24T06:06:22.000Z', amount: 100.42 },
+        { date: '2022-12-12T08:42:10.000Z', amount: -100.42 }
     ]
     const resultBalance: Balance = {
         currentBalance: 0,
@@ -19,8 +19,26 @@ describe('Transactions', () => {
         ]
     }
 
+    const balance = transformTransactions(transactions)
+
     it('should transform transactions to monthly balance', async () => {
-        const months = transformTransactions(transactions)
-        expect(months).toEqual(resultBalance)
+        expect(balance.monthlyBalance).toEqual(resultBalance.monthlyBalance)
+    })
+
+    it('should calculate the total current balance correctly', () => {
+        expect(balance.currentBalance).toBe(resultBalance.currentBalance)
+    })
+
+    it('should calculate multiple transactions in single month correctly', () => {
+        const transactions: Transaction[] = [
+            { date: '2022-10-02T06:02:22.000Z', amount: 400.1 },
+            { date: '2022-10-13T14:10:12.000Z', amount: -400.1 },
+            { date: '2022-10-24T16:23:42.000Z', amount: 100.42 },
+            { date: '2022-10-29T10:14:17.000Z', amount: 0.58 }
+        ]
+        const balance = transformTransactions(transactions)
+
+        const monthBalance = [{ date: { year: 2022, month: 10 }, balance: 101.0 }]
+        expect(balance.monthlyBalance).toEqual(monthBalance)
     })
 })
