3a17f9f63b1c98612498b99482f27447bd3e34aa
Roman <romanranniew@gmail.com>
2023-08-24 20:40:23 +0200

    Add quasar ui framework

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,19 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import App from './App.vue'
 import { formatCurrency } from './common/directives/format'
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+
+import App from './App.vue'
 
 const app = createApp(App)
 
+app.use(Quasar, {
+    plugins: {} // import Quasar plugins and add here
+})
 app.use(createPinia())
-app.mount('#app')
-
 app.directive('balance', formatCurrency)
+
+app.mount('#app')
